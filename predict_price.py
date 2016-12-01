938684ed133dce4d2bd8890c718ce55c5d415f31
bgts <390676317@qq.com>
2016-12-02 00:33:58 +0800

    Update predict_price.py
    After several tests, cubic spline is confirmed as the most suitable interpolate mothod for this data.

--- predict_price.py
+++ predict_price.py
@@ -6,6 +6,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 import datetime
 from statsmodels.tsa.arima_model import ARMA
+from scipy import interpolate
+from old_TS import time_sequence_predict
+import decimal
 #删掉原训练集、测试集文件的第一行
 def readAsChunks(file_dir, types):
     chunks = []
@@ -25,6 +28,7 @@ def readAsChunks(file_dir, types):
     df = pd.concat(chunks, ignore_index=True)
     #分块将.txt文件读入内存，放到一个 pandas 的 dataFrame 里。块大小（即每次读的行数）为chunk_size
     return df
+'''
 def readAsDic(file_dir):
     dict_data = {}
     with open(file_dir, 'r') as df:
@@ -34,72 +38,127 @@ def readAsDic(file_dir):
             dict_data[(kv[1],kv[3])].append(pd.to_datetime(kv[9],errors='coerce'))
     return dict_data
     
-dic = readAsDic("product_market.csv")
-
-df = readAsChunks("farming.csv", {8:np.float32, 9:np.float32,10:np.float32})#.replace("\N",np.nan)
+dic = readAsDic("product_market.csv")#读入测试集文件 将测试集存成字典 key:(市场映射值,产品映射值) value:待预测的日期list
+'''
+df = readAsChunks("farming.csv", {8:np.float32, 9:np.float32,10:np.float32})
 #数据发布时间 对于格式有问题的时间设为NaT
 df[12] = pd.to_datetime(df[12],errors='coerce')
 
-#留下第0,1,2,3,9,12列
+#留下训练集的第0,1,3,9,12列
 df.drop([4,5,6,7,8,10,11],axis=1,inplace=True)
 #读测试数据
-df_test = readAsChunks("product_market.csv",{})#.replace("null",np.nan)
+df_test = readAsChunks("product_market.csv",{})
 #数据发布时间 对于格式有问题的时间设为NaT
-df_test[9] = pd.to_datetime(df_test[9],errors='coerce')
-#留下第0,1,2,3,9列
-df_test.drop([4,5,6,7,8],axis=1,inplace=True)
+df_test[4] = pd.to_datetime(df_test[4],errors='coerce')
+
 #提取测试集中的市场名称映射值和农产品名称映射值
-df_used = df_test.drop([0,2,9],axis=1,inplace=False)
-df_used = df_used.drop_duplicates()
-#提取根据测试集中的（市场名称映射值,农产品名称映射值），在训练集中提取相应的行
-print 'ok'
-df = pd.merge(df,df_used,on=[1,3])
-df_used = []
-x = []
-y = []
+df_test = df_test.drop([0,4],axis=1,inplace=False)
+df_test = df_test.drop_duplicates()
+#根据测试集中的（市场名称映射值,农产品名称映射值），在训练集中提取相应的行
+df = pd.merge(df,df_test,how='inner',on=[1,2,3])
 
-print 'ok1'
-df=df.sort_values(by=[1, 3],ascending=[1, 1])
-df_test=df_test.sort_values(by=[1, 3],ascending=[1, 1])
 
-#df.columns = [0, 1, 2, 3, 9, 12, 13]
-key_tmp = ('02AAD134CD776815520A00CDC36A61E1','076095B1B9B448BF166FEB8A0EF80E83')
-print 'ok2'
-d1 = datetime.datetime(2015, 1, 1)
-predict_time = pd.to_datetime(['2016-01-28'])
-predict_time = (predict_time-d1).days%365
-#print predict_time
 
-for index, row in df.iterrows():   # 获取每行的index、row
-    if key_tmp==(row[1],row[3]):
-        x.append(pd.to_datetime(row[12]))
-        y.append(row[9])
-    else:
-        #x = np.array(x)
-        #y = np.array(y)
-        print x
-        print y
-        y=pd.Series(y)
-        y.index = pd.Index(x)
-        start = datetime.datetime(2015,8,5)
-        end = datetime.datetime(2016,1,21)
-        print start
-        arma =ARMA(y, order =(2,0))
-        results = arma.fit()
-        a = results.predict('2015-8-5','2016-1-21', dynamic=True)
-        print a
-        break
-        '''
-        print x
-        print y
-        clf = Pipeline([('poly', PolynomialFeatures(degree=7)),  
-                    ('linear', LinearRegression(fit_intercept=False))])  
-        clf.fit(x[:, np.newaxis], y)
-        y_test = clf.predict(predict_time[:, np.newaxis])
-        print y_test
+#对训练集数据按（市场名称映射值,种类,农产品名称映射值）排序
+df=df.sort_values(by=[1, 3, 2],ascending=[1, 1, 1])
+#对测试集数据按（市场名称映射值,农产品名称映射值）排序
+#df_test=df_test.sort_values(by=[1, 3],ascending=[1, 1])
+
+#初始化
+x = []#存日期
+y = []#存农产品价格
+key_tmp = ('02AAD134CD776815520A00CDC36A61E1','蔬菜','076095B1B9B448BF166FEB8A0EF80E83')
+#排序后训练集中的第一个（市场名称映射值,农产品名称映射值）二元组
+#key_tmp存（市场名称映射值,农产品名称映射值）二元组
+last_date = datetime.datetime(2016,6,30)
+predict_dates = pd.date_range(datetime.datetime(2016,7,1), periods=31).tolist()
+file_object = open('result.csv','w')
+count=0
+count_total=0
+for index, row in df.iterrows():   # 获取训练集中每行的索引和内容
+    if key_tmp!=(row[1],row[2],row[3]):#比较当前行的第1列和第3列组成的二元组是否和key_tmp一致
+        #用线性插值补全数据
+        data = [(score, name) for score, name in zip(x,y)]
+        data.sort() 
+        x=[score for score,name in data] #将排好序的分数姓名的元组分开
+        y=[name for score,name in data]
+        base = min(x)
+        ceiling = max(x)
+        x_full_dates = pd.date_range(base, periods=(ceiling-base).days+1, freq='D').tolist()
+        x_interpld = [(i-base).days for i in x]
+        x_interpld = np.array(x_interpld)
+        y = np.array(y)
+        s1rev = interpolate.CubicSpline (x_interpld, y)
+        #s1rev = interpolate.UnivariateSpline (x_interpld, y)
+        #s1rev = interpolate.InterpolatedUnivariateSpline(x_interpld, y)
+        #s1rev = interpolate.Akima1DInterpolator(x_interpld, y)
+        #s1rev = interpolate.PchipInterpolator(x_interpld, y)
+        #s1rev = interpolate.interp1d(x_interpld, y)
+        
+        #tck = interpolate.splrep(x_interpld, y)
+        x_full_days = [(i-base).days for i in x_full_dates]
+        x_full_days = np.array(x_full_days)
+        y_interp = s1rev(x_full_days)
+        #y_interp = interpolate.splev(x_full_days, tck)
+        ceiling = max(x_full_dates)
+        if last_date != ceiling:
+            x_compensate = pd.date_range(ceiling + datetime.timedelta(days=1), periods=(last_date-ceiling).days, freq='D').tolist()
+            x_full_dates.extend(x_compensate)
+            y_compensate = [y_interp[-1] for i in range(len(x_compensate))]
+            y_interp = y_interp.tolist()
+            y_interp.extend(y_compensate)
+        x = x_full_dates
+        y = y_interp
+        y = pd.Series(y)#将list类型转换成series，不然做不了时间序列
+        y.index = pd.Index(x)#将日期作为农产品价格的索引
+        #print y
+        try:
+            predict_series = time_sequence_predict(y)
+            y=y.append(predict_series)
+        
+        except Exception,e:  #若时间序列有错 用滚动平均值预测
+            print Exception,":",e
+            count+=1
+            i = 6
+            sum_price = 0
+            for j in range(1,i+1)[::-1]:
+                try:
+                    sum_price += y[-j]
+                except:
+                    i -= 1
+                    continue
+            a = sum_price/i
+            for date in predict_dates:
+                s = pd.Series([a], index=[date])
+                y=y.append(s)
+                a = (sum_price - y[-i-1] + a)/i                
+        
+        for date in predict_dates:
+            splt = str(date).split(' ')
+            string = str(key_tmp[0])+","+str(key_tmp[1])+","+str(key_tmp[2])+","+str(splt[0])+","+str(round(decimal.Decimal(y[date]*10))/10)+'\n'
+            file_object.write(string)
         x = []
         y = []
-        key_tmp = (row[1],row[3])
-        predict_time = (pd.to_datetime(row[13])-d1).days%365
-        print predict_time
-        '''
+        key_tmp = (row[1],row[2],row[3])
+        count_total+=1
+        #break
+    x.append(pd.to_datetime(row[12]))#将当前行的第12列(日期)加入x
+    y.append(row[9])#将当前行的第9列(农产品价格)加入x
+data = [(score, name) for score, name in zip(x,y)]
+data.sort()  
+x=[score for score,name in data] #将排好序的分数姓名的元组分开
+y=[name for score,name in data]
+try:
+    a=(y[-5]+y[-4]+y[-3]+y[-2]+y[-1])/5
+except:
+    print key_tmp
+    
+try:
+    for date in predict_dates:
+        splt = str(date).split(' ')
+        string = str(key_tmp[0])+","+str(key_tmp[1])+","+str(key_tmp[2])+","+str(splt[0])+","+str(round(decimal.Decimal(a*10))/10)+'\n'
+        file_object.write(string)
+except:
+    file_object.close()
+file_object.close()
+print count,count_total
